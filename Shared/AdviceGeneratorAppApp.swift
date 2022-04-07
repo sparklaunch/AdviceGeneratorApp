faa151e969da6948ebf56395512a5228920f58b3
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-07 13:01:54 +0900

    Initial Commit

---
+++ Shared/AdviceGeneratorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdviceGeneratorAppApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/04/07.
+//
+
+import SwiftUI
+
+@main
+struct AdviceGeneratorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
